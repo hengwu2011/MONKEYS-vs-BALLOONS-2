344ba7d1a71c8a46cc30d74a6e3d851107ff8be7
Hsing <hsingwu27@gmail.com>
2024-07-11 16:50:10 -0700

    created tower.py and described a tower class

---
+++ tower.py
@@ -0,0 +1,20 @@
+"""on click creates a tower
+towers should be able to have
+health,
+damage,
+attack speed,
+x , y
+size
+"""
+"""tower actions
+spawn projectiles
+spawn currency 
+"""
+
+class Towers:
+    damage = 5
+
+# Create an object of MyClass
+
+# Access the value of 'x' in the object
+  # Output: 5
