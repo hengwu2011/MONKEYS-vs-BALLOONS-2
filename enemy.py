91dc0411efa01c518ffa469b8ff05a30544502d9
heng <hengwu2011@gmail.com>
2024-07-11 16:50:13 -0700

    added enemy notes and a class and method

---
+++ enemy.py
@@ -0,0 +1,11 @@
+class Enemy:
+  pass
+'''
+Heath,position, speed, size, sprites, damage value
+Need to know how to move 
+
+Speed⚡/Damage➖/Health❤️ are different for each enemy/different sprite
+
+'''
+def prop(self):
+  p1 = Enemy("type of enemy")
