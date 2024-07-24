845f1b831f8a1325ade8c11bdbc4b97e91497446
Hsing <hsingwu27@gmail.com>
2024-07-23 17:11:33 -0700

    upon click rectangles will spawn on a grid

--- list.py
+++ list.py
@@ -1,5 +1,17 @@
+"GOAl"#get towers to appear on grid upon click
+"""ingredients: 
+x and y of rect??, mouse button  up event,function to draw rectangles"""
+"""translation: 
+x and y of rect??
+if event.type == MOUSEBUTTONUP:
+    pg.draw.rect(grid rect x, grid rect y,)
+"""
+#!PROBLEM how to get x and y values of a rectangle on a grid?
+
 import pygame
-import sys
+import math
+def quantize(mouse_x_pos,grid_x_size):
+    return math.floor(mouse_x_pos/grid_x_size)*grid_x_size + grid_x_size/2
 
 # Initialize pygame
 pygame.init()
@@ -28,10 +40,16 @@ def draw_grid():
 # Main game loop
 while True:
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONUP:
+            rect_x = quantize(pygame.mouse.get_pos()[0],100)
+            rect_y = quantize(pygame.mouse.get_pos()[1],100)
+            R = pygame.Rect(rect_x,rect_y,100,100)
+            pygame.draw.rect(screen, "green", R)
         if event.type == pygame.QUIT:
+            
             pygame.quit()
-            sys.exit()
 
-    screen.fill(BLACK)
-    draw_grid()
+
+    
+    
     pygame.display.update()
