1ff561f002ba6a32de9c64cc87a22daf16e08c97
heng <hengwu2011@gmail.com>
2024-08-09 17:40:18 -0700

    made a background for the game

--- main.py
+++ main.py
@@ -59,14 +59,17 @@ def draw_grid():
             rect = pg.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
             pg.draw.rect(screen, WHITE, rect, 1)
 
+bg = pg.image.load("assets/Background/grasslanes.jpg")
+bg_scaled = pg.transform.scale(bg, (WINDOW_WIDTH, WINDOW_HEIGHT))
+
 while True:
     screen.fill("black")
     draw_grid()
+    screen.blit(bg_scaled, (0,0))
     show_sprite(tower_list)
     for x in enemy_list:
         x.skin()
         x.movement()
-        
     for event in pg.event.get():
         if event.type == pg.MOUSEBUTTONDOWN:
             rect_x = quantize(pg.mouse.get_pos()[0],BLOCK_SIZE,BORDER)
